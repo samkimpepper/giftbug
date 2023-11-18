14db91808d71283c5d2148bb6c673b93b344ab8a
samkimpepper <vegielcl@gmail.com>
2023-11-18 17:21:38 +0900

    [fix] EC2에서 프로메테우스 설정

--- src/main/java/com/pretchel/pretchel0123jwt/Pretchel0123JwtApplication.java
+++ src/main/java/com/pretchel/pretchel0123jwt/Pretchel0123JwtApplication.java
@@ -2,9 +2,14 @@ package com.pretchel.pretchel0123jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 
